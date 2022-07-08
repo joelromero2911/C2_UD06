af90d4795d2a547f361f15df03cc1ba55c956234
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-08 12:05:35 +0200

    Commit UD06 04

---
+++ C2_UD06_04/src/UD06_04_App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class UD06_04_App {
+
+	public static void main(String[] args) {
+		
+//		4) Crea una aplicación que nos calcule el factorial de un número pedido por teclado, lo
+//		realizara mediante un método al que le pasamos el número como parámetro. Para calcular
+//		el factorial, se multiplica los números anteriores hasta llegar a uno. Por ejemplo, si
+//		introducimos un 5, realizara esta operación 5*4*3*2*1=120:
+		
+//		Variables
+		Scanner sc = new Scanner(System.in);
+		
+//		Programa
+		System.out.print("Dime que numero quieres calcular el factorial: ");
+		int num = Integer.parseInt(sc.nextLine());
+		sc.close();
+		
+		calcularFactorial(num);
+
+	}
+	
+	public static void calcularFactorial(int num) {
+		
+		int fact = 1;
+		
+		while (num != 0) {
+			fact *= num--;
+		}
+		
+		System.out.println("El factorial de numero introducido es: " + fact);
+	}
+
+}
