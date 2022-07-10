a0c53e885c150d81dbb573e12f09746ddb8c7930
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-10 02:19:06 +0200

    Ajuste para seguir el enunciado

--- C2_UD06_09/src/UD06_09_App.java
+++ C2_UD06_09/src/UD06_09_App.java
@@ -8,6 +8,11 @@ public class UD06_09_App {
 //		rellenaremos el array con números aleatorios entre 0 y 9. Al final muestra por pantalla el
 //		valor de cada posición y la suma de todos los valores.
 		
+//		Tareas a realizar: Haz un método para rellenar el array(que tenga como parámetros los
+//		números entre los que tenga que generar) y otro para mostrar el contenido y la suma del
+//		array.
+
+		
 //		Variables
 		Scanner sc = new Scanner(System.in);
 		
@@ -17,26 +22,33 @@ public class UD06_09_App {
 		
 		sc.close();
 		
-		rellenarSumarMostrar(cantidad);
+		int arrayNum[] = new int [cantidad];
+		arrayNum = rellenarArray(cantidad);
+		sumarMostrar(arrayNum);
 
 	}
 	
-//	Crea un array tan grande como se le indique por parametro, lo rellena con números aleatorios entre 0 y 9,
-//	muestra las posiciones del array con sus valores por pantalla, hace la suma de total de todos los números y
-//	la muestra por pantalla.
-	public static void rellenarSumarMostrar(int cantidad) {
+	public static int[] rellenarArray(int cantidad) {
 		
 		int arrayNum[] = new int [cantidad];
-		int suma = 0;
 		
 		for (int i = 0; i < arrayNum.length; i++) {
 			arrayNum[i] = (int) (Math.random() * (9 - 0)) + 0;
+		}
+		
+		return arrayNum;
+	}
+	
+	public static void sumarMostrar(int[] arrayNum) {
+		
+		int suma = 0;
+		
+		for (int i = 0; i < arrayNum.length; i++) {
 			suma += arrayNum[i];
 			System.out.println("Array de numeros [" + i + "] = " + arrayNum[i]);
 		}
 		
 		System.out.println("Suma de todos los numeros: " + suma);
-
 	}
 
 }
